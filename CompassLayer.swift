71222697573f758def7feb7ad1364512162ecfb5
thierryH91200 <thierry.hentic@gmail.com>
2017-08-09 11:44:59 +0200

    bug major

--- CompassLayer.swift
+++ CompassLayer.swift
@@ -103,11 +103,13 @@ class CompassLayer: NSView {
     }
     
     func DegreesToRadians (_ value:CGFloat) -> CGFloat {
-        return value * .pi / 180.0
+        let radians = CGFloat(value * .pi / 180.0)
+        return radians
     }
     
     func RadiansToDegrees (_ value:CGFloat) -> CGFloat {
-        return value * 180.0 / .pi
+        let degrees = CGFloat(value * 180.0 / .pi)
+        return degrees
     }
     
     func setImage(name: String)->CALayer
